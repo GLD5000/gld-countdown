c28d9828d4ea33fa582a5f48e1bb298b434a5f61
Gareth Devlin <garethlouisdevlin@gmail.com>
2025-07-15 14:03:00 +0100

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,6 @@
-var __create = Object.create;
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
-var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __export = (target, all) => {
   for (var name in all)
@@ -16,14 +14,6 @@ var __copyProps = (to, from, except, desc) => {
   }
   return to;
 };
-var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
-  // If the importer is in node compatibility mode or this is not an ESM
-  // file that has been converted to a CommonJS file using a Babel-
-  // compatible transform (i.e. "__esModule" has not been set), then set
-  // "default" to the CommonJS "module.exports" for node compatibility.
-  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
-  mod
-));
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
 // src/index.ts
@@ -72,8 +62,70 @@ function makeTimer(deadlineString) {
 }
 
 // src/countdownString.ts
-var fs = __toESM(require("fs"));
-var string = fs.readFileSync("./src/countdown.mjs", "utf8").replaceAll("export ", "");
+var string = `/**
+ * 
+ *
+ * @param {number} deadlineMs 
+ * @returns {{ days: number; hours: number; minutes: number; seconds: number; }} 
+ */
+function getUnitsRemaining(deadlineMs) {
+  const secondsRemaining = getSecondsRemaining(deadlineMs);
+  if (secondsRemaining < 1)
+    return {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+  const seconds = secondsRemaining % 60;
+  const minutes = Math.floor((secondsRemaining / 60) % 60);
+  const hours = Math.floor(secondsRemaining / 60 / 60) % 24;
+  const days = Math.floor(secondsRemaining / 60 / 60 / 24);
+
+  return {
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+}
+
+/**
+ * 
+ *
+ * @param {string} deadlineString 
+ * @returns {number} 
+ */
+function parseDeadline(deadlineString) {
+  const deadlineMs = Date.parse(deadlineString);
+  return deadlineMs;
+}
+
+/**
+ * 
+ *
+ * @param {number} deadlineMs 
+ * @returns {number} 
+ */
+function getSecondsRemaining(deadlineMs) {
+  const now = Date.now();
+  const differenceMs = deadlineMs - now;
+  return Math.floor(differenceMs * 0.001);
+}
+
+/**
+ * 
+ *
+ * @export
+ * @param {string} deadlineString 
+ * @returns {() => { days: number; hours: number; minutes: number; seconds: number; }} 
+ */
+export function makeTimer(deadlineString) {
+  const deadlineMs = parseDeadline(deadlineString);
+  return () => {
+    return getUnitsRemaining(deadlineMs);
+  };
+}`.replaceAll("export ", "");
 var countdownString = string;
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
--- dist/index.mjs
+++ dist/index.mjs
@@ -36,8 +36,70 @@ function makeTimer(deadlineString) {
 }
 
 // src/countdownString.ts
-import * as fs from "fs";
-var string = fs.readFileSync("./src/countdown.mjs", "utf8").replaceAll("export ", "");
+var string = `/**
+ * 
+ *
+ * @param {number} deadlineMs 
+ * @returns {{ days: number; hours: number; minutes: number; seconds: number; }} 
+ */
+function getUnitsRemaining(deadlineMs) {
+  const secondsRemaining = getSecondsRemaining(deadlineMs);
+  if (secondsRemaining < 1)
+    return {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+  const seconds = secondsRemaining % 60;
+  const minutes = Math.floor((secondsRemaining / 60) % 60);
+  const hours = Math.floor(secondsRemaining / 60 / 60) % 24;
+  const days = Math.floor(secondsRemaining / 60 / 60 / 24);
+
+  return {
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+}
+
+/**
+ * 
+ *
+ * @param {string} deadlineString 
+ * @returns {number} 
+ */
+function parseDeadline(deadlineString) {
+  const deadlineMs = Date.parse(deadlineString);
+  return deadlineMs;
+}
+
+/**
+ * 
+ *
+ * @param {number} deadlineMs 
+ * @returns {number} 
+ */
+function getSecondsRemaining(deadlineMs) {
+  const now = Date.now();
+  const differenceMs = deadlineMs - now;
+  return Math.floor(differenceMs * 0.001);
+}
+
+/**
+ * 
+ *
+ * @export
+ * @param {string} deadlineString 
+ * @returns {() => { days: number; hours: number; minutes: number; seconds: number; }} 
+ */
+export function makeTimer(deadlineString) {
+  const deadlineMs = parseDeadline(deadlineString);
+  return () => {
+    return getUnitsRemaining(deadlineMs);
+  };
+}`.replaceAll("export ", "");
 var countdownString = string;
 export {
   countdownString,
